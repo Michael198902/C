3c2bc988481f2e9719bfc776ac8f84365a7618af
Michael198902 <56581842+Michael198902@users.noreply.github.com>
2019-10-23 22:54:29 +1100

    Delete UART2.h

--- UART/UART2.h
+++
@@ -1,9 +0,0 @@
-
-#define lineU2()		putU2('\r');putU2('\n')
-
-//---------------- Function prototypes ---------------------
-void initU2(void);	 	
-void putU2(int c);	 	
-void putsU2(char *s); 	
-char getU2(void);	 	
-
